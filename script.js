8f102fc8bb1f03e4b3d4d73b473de99518d4f510
Aswaglord <62083663+Aswaglord@users.noreply.github.com>
2021-05-02 15:55:34 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector("td").addEventListener("click", handleClick);
+
+let player1 = "O";
+let player2 = "X";
+function handleClick() {
+    
+    document.querySelector("td") = "X";
+} 
